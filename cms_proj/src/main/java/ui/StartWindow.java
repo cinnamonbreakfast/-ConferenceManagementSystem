d41909e23bea567700dfcc50b93108e8c8739f40
Andrew C <candetandrei@gmail.com>
2020-05-14 22:43:13 +0300

    Project GUI start

--- cms_proj/src/main/java/ui/StartWindow.java
+++ cms_proj/src/main/java/ui/StartWindow.java
@@ -1,20 +1,30 @@
 package ui;
 
+import com.sun.tools.javac.Main;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.layout.*;
+import javafx.util.Duration;
 
+import javax.naming.PartialResultException;
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class StartWindow extends Application {
 
-    @FXML public AnchorPane anchpane;
-    @FXML private VBox mainLayout;
+    @FXML public Button loginBtn;
+    @FXML private AnchorPane anchpane;
+    @FXML public TextField login_user_textbox;
+    @FXML public ImageView loginHero;
 
     public static void main(String[] args) {
         launch(args);
@@ -28,14 +38,42 @@ public class StartWindow extends Application {
 //        primaryStage.show();
 
         FXMLLoader root = new FXMLLoader(getClass().getResource("/StartWindow.fxml"));
-        AnchorPane anchorPane = root.load();
+        anchpane = root.load();
+        anchpane.getStylesheets().add("ui/forms/basic_forms.css");
 
-        Scene scene = new Scene(anchorPane, 700, 500);
-
-        mainLayout.maxHeight(100);
+        Scene scene = new Scene(anchpane, 600, 485);
 
         stage.setTitle("CMS");
         stage.setScene(scene);
         stage.show();
+
+        
+
+
+    }
+
+    @FXML
+    void loginUser(ActionEvent event) {
+        try {
+            FXMLLoader root2 = new FXMLLoader(MainWindow.class.getResource("/MainWindow.fxml"));
+            Parent parent = (Parent) root2.load();
+
+            Stage new_stage = new Stage();
+            new_stage.setTitle("CMS 2");
+            new_stage.setScene(new Scene(parent));
+            new_stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void initialize(){
+        TranslateTransition floatAnimation = new TranslateTransition(Duration.millis(3000), loginHero);
+        floatAnimation.setFromY(loginHero.getTranslateY());
+        floatAnimation.setToY(loginHero.getTranslateY()+20);
+        floatAnimation.setCycleCount(Timeline.INDEFINITE);
+        floatAnimation.setAutoReverse(true);
+        floatAnimation.play();
     }
 }
